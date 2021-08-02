79dbb438228da4de2112c535060f8151866ac667
trieule50 <trieule50@gmail.com>
2021-08-02 15:11:02 -0400

    Add Home and Nav Component

---
+++ src/components/Home/index.js
@@ -0,0 +1,16 @@
+const Home = () =>{
+    return(
+        <div>
+            <div>
+                <h1>Welcome to Speech Audit</h1>
+                <h8>Understand how others perceive what you say.</h8>
+            </div>
+            <div>
+                <img src='https://image.flaticon.com/icons/png/512/1141/1141031.png' alt='people speaking'/>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,22 @@
+import { Container, Navbar, Nav } from "react-bootstrap";
+
+const Navigation = () => {
+    return(
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="/"><img src='https://image.flaticon.com/icons/png/512/4359/4359295.png' alt='speech audit' style={{height: '2rem'}}/> Speech Audit</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/demo">Demo</Nav.Link>
+                        <Nav.Link href="/signup">Sign Up</Nav.Link>
+                        <Nav.Link href="/signin">Sign In</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Navigation
