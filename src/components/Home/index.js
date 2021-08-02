67f435f7ce0dae63ac1ffa48116158c56441fcdc
trieule50 <trieule50@gmail.com>
2021-08-02 16:12:03 -0400

    Add Styles on Home Component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,13 @@
 const Home = () =>{
     return(
-        <div>
-            <div>
+        <div className='home-container'>
+            <div className='home-item'>
                 <h1>Welcome to Speech Audit</h1>
                 <h8>Understand how others perceive what you say.</h8>
             </div>
             <div>
-                <img src='https://image.flaticon.com/icons/png/512/1141/1141031.png' alt='people speaking'/>
-            </div>
-
+                <img className='home-img' src='https://image.flaticon.com/icons/png/512/1141/1141031.png' alt='people speaking'/>
+            </div>   
         </div>
     )
 }
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,7 +2,7 @@ import { Container, Navbar, Nav } from "react-bootstrap";
 
 const Navigation = () => {
     return(
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="light" expand="md">
             <Container>
                 <Navbar.Brand href="/"><img src='https://image.flaticon.com/icons/png/512/4359/4359295.png' alt='speech audit' style={{height: '2rem'}}/> Speech Audit</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
