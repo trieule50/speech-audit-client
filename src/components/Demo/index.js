d78935adc2f40884fc3e68c8cbc588177561088e
trieule50 <trieule50@gmail.com>
2021-11-01 19:05:05 -0400

    Add demo picture and added styling

--- src/components/Demo/index.js
+++ src/components/Demo/index.js
@@ -65,13 +65,15 @@ const Demo = () => {
 
     return(
         <div className='demo'>
-          <div>
+          <div className='demo-img'>
+            <div className='demo-text'>
             <h1>About Speech Audit</h1>
             <p>Helping others to understand tone.</p>
+            </div>
           </div>
           <div>
             <p>
-              Tone is the general character or attitude of a place, piece of writting, situation, etc and can helps clarifies and conveys meaning. Similar to putting ones best foot foward, understanding your tone or someone else can affect how people perceive you. Speech Audit's mission is to help other understand tone. 
+              Tone is the general character or attitude of a place, piece of writting, situation, etc and can helps clarifies and conveys meaning. Similar to putting ones best foot foward, understanding your tone or someone else can affect how people perceive you. Speech Audit's mission is to help others understand tone with a click of a button. 
             </p>
             <blockquote>
               "10% of conflict is due to difference of opinion and 90% is due to delivery and tone of voice."
