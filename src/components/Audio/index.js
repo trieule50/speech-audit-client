dcc708c3bccdf56435371df9b82bf8ffd1e3968d
trieule50 <trieule50@gmail.com>
2021-11-02 17:58:20 -0400

    Update speech analyzer page

--- src/components/Audio/index.js
+++ src/components/Audio/index.js
@@ -3,21 +3,18 @@ import { Form, Button } from 'react-bootstrap'
 const Audio = () => {
     return(
         <div className='audio'>
-            <div className='audio-item countdown'>
-            </div>
             <div className='audio-item'>
                 <div className='logo'>
                 <img src='https://image.flaticon.com/icons/png/512/4359/4359295.png' alt='speech audit' style={{height: '1.5rem'}}/>
                 <p>Speech Audit</p>
                 </div>
                 <p className='coming-soon'>GREAT THINGS ARE COMING</p>
+                <p>Target date is December 2021</p>
                 <Button href='/demo' variant="warning" style={{ marginBottom: '2rem'}}>
                 Learn More {">>"}
                 </Button>
             </div>
         </div>
-
-
 
         // <div className='audio'>
         //     <h2>Audio Analyzer</h2>
