50e2594f247b3f3221524091dd5957761f5e9cf8
trieule50 <trieule50@gmail.com>
2021-11-01 23:17:56 -0400

    Edit word cloud background

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,13 +4,17 @@ import words from "./words";
 import 'tippy.js/dist/tippy.css';
 import 'tippy.js/animations/scale.css';
 
+const callbacks = {
+    getWordColor: word => word.value > 5 ? "yellow" : "white"
+  }
+
 const Home = ({ userInfo }) =>{
     return(
         <div className='home-container'>
-            <div className='home-item' style={{ height: 450, width: "100%" }}>
-                <ReactWordcloud words={words}/>
+            <div className='home-item cloud' style={{ height: 450, width: "100%" }}>
+                <ReactWordcloud words={words} callbacks={callbacks}/>
             </div>
-            <div className='home-item'>
+            <div className='home-item opening'>
                 <div>
                 <h1>Welcome
                     {userInfo
