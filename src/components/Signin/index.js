fc8dbb412cb86eb5600bdff39e2c7f78b672bf4a
trieule50 <trieule50@gmail.com>
2021-11-02 11:34:53 -0400

    Update sign in page

--- src/components/Signin/index.js
+++ src/components/Signin/index.js
@@ -47,11 +47,20 @@ const Signin = ({ setUserInfo, handleSetLoggedIn }) => {
     }
     return(
         <div className='signin'>
-            <h2>Sign In</h2>
+            <div className='signin-item signin-img'>
+                <img className='signin-img' src='https://earimediaprodweb.azurewebsites.net/Api/v1/Multimedia/c8fd9bde-38ac-4fc6-bf3f-3246b84748a9/Rendition/low-res/Content/Public'/>
+            </div>
+            <div className='signin-item'>
+            <div className='logo'>
+            <img src='https://image.flaticon.com/icons/png/512/4359/4359295.png' alt='speech audit' style={{height: '1.5rem'}}/>
+            <p>Speech Audit</p>
+            </div>
+            <h2>Welcome Back</h2>
+            <small>Sign in to stay updated on our speech analyzer</small>
             <Container className='signin-form'>
-            <Form onSubmit={_handleSignin}>
+            <Form onSubmit={_handleSignin} style={{width: "20rem", margin: '2rem'}}>
                 <Form.Group className="mb-3" controlId="email">
-                    <Form.Label>Email address</Form.Label>
+                    {/* <Form.Label>Email address</Form.Label> */}
                     <Form.Control 
                     type="email" 
                     value={signinData.email}
@@ -59,9 +68,8 @@ const Signin = ({ setUserInfo, handleSetLoggedIn }) => {
                     onChange={_handleChange} 
                     />
                 </Form.Group>
-
                 <Form.Group className="mb-3" controlId="password">
-                    <Form.Label>Password</Form.Label>
+                    {/* <Form.Label>Password</Form.Label> */}
                     <Form.Control 
                     type="password" 
                     value={signinData.password}
@@ -70,11 +78,13 @@ const Signin = ({ setUserInfo, handleSetLoggedIn }) => {
                     />
                 </Form.Group>
                 <Button variant="primary" type="submit">
-                    Submit
+                    Sign In
                 </Button>
                 {error && <Alert variant='danger'>Oops, something went wrong. Please try again.</Alert>}
             </Form>
             </Container>
+            </div>
+            
         </div>
     )
 }
